ae2d437fda731f99624bb2497e9a78a4254ecf3d
Nikita Ryabov <xleo77777@gmail.com>
2023-06-25 11:10:28 +0300

    Gorshok

--- Assets/Scenes/Rocket.cs
+++ Assets/Scenes/Rocket.cs
@@ -5,31 +5,53 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+
+    Rigidbody rigidBody;
+    AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
         ProcessInput();
+        ProcessSound();
     }
 
+    private void ProcessSound()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Pause();
+        }
+    }
     private void ProcessInput()
     {
         if (Input.GetKey(KeyCode.Space))
         {
+            rigidBody.AddRelativeForce(Vector3.up);
             print("Space pressed");
         }
         if (Input.GetKey(KeyCode.A))
         {
             print("Left rotating");
+            transform.Rotate(Vector3.forward);
         }
         if (Input.GetKey(KeyCode.D))
         {
             print("Right rotating");
+            transform.Rotate(-Vector3.forward);
         }
     }
 }
